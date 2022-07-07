254bdf576c77699dc0dbc163fc7d986a644e2526
Illia Fedotov <illia.fedotov@ideasoft.io>
2022-07-07 15:21:51 +0300

    add server && api for client

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,2 +1,18 @@
 export * from './client'
 
+import http from "http"
+
+import { getStakingPageInfo, getAmountAndSupply } from "./controller"
+
+const myServer = http.createServer((req, res) => {
+  if (req.method == "GET" && req.url == "/api/staking-page-info") {
+    return getStakingPageInfo(req, res)
+  }
+  if (req.method == "GET" && req.url == "/api/amount-supply") {
+    return getAmountAndSupply(req, res)
+  }
+})
+
+myServer.listen(3000, () => {
+  console.log("Server is running on port 3000")
+})
