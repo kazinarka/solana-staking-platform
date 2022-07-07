151d0743e395298c7347ce77eefd19dcc9caea0b
Illia Fedotov <illia.fedotov@ideasoft.io>
2022-07-07 14:32:49 +0300

    add instruction calls for tests && refactor reward calculation

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,17 +1,17 @@
-// export * from './client'
+export * from './client'
 
-import http from "http";
-
-import {getTasks} from "./controller";
-
-const myServer = http.createServer((req, res) => {
-    if (req.method == "GET" && req.url == "/api/tasks") {
-        return getTasks(req, res)
-    }
-});
-
-myServer.listen(3000, () => {
-    console.log('Server is running on port 3000')
-});
-
-myServer.close()
+// import http from "http";
+//
+// import {getTasks} from "./controller";
+//
+// const myServer = http.createServer((req, res) => {
+//     if (req.method == "GET" && req.url == "/api/tasks") {
+//         return getTasks(req, res)
+//     }
+// });
+//
+// myServer.listen(3000, () => {
+//     console.log('Server is running on port 3000')
+// });
+//
+// myServer.close()
