756603485999dfe849e4772ead2d3ceaa46ea8fe
Illia Fedotov <illia.fedotov@litslink.com>
2022-10-24 10:33:32 +0300

    clear info

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -5,6 +5,6 @@ pub mod instruction;
 pub mod processor;
 pub mod state;
 
-solana_program::declare_id!("");
+solana_program::declare_id!("GyC8iyGUyVxM9ovGw6DBpPnXLWXw6aAeXB2A8SEVqnN3");
 
 pub type Timestamp = u64;
